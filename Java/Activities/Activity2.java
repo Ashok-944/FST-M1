240d4f1a8c360898155796e8b4b5fe58dea4f005
Ashok-944 <naikashok1818@gmail.com>
2025-07-23 13:20:15 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,21 @@
+package Activity;
+
+public class Activity2 {
+
+	public static void main(String[] args) {
+		int[] numbers = {10, 77, 10, 54, -11, 10};
+        int sum = 0;
+        for (int number : numbers) {
+            if (number == 10) {
+                sum += number;
+            }
+        }
+        if (sum == 30) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+	}
+
+}
