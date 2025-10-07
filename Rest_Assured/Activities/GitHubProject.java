7a8021ff336cbfcc2d831c14bf4aa32fc9db013f
Ashok-944 <naikashok1818@gmail.com>
2025-10-07 11:42:20 +0530

    GitHubProjectTest.java

--- Rest_Assured/Activities/GitHubProject.java
+++ Rest_Assured/Activities/GitHubProject.java
@@ -1,10 +1,9 @@
-package project;
 
-import java.io.IOException;
+package GitHubProject;
+
 import java.util.HashMap;
 
 import org.hamcrest.Matchers;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -15,69 +14,67 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-public class GitHubProject {
-	
+public class GitHubProjTest {
+	/*
+	 * POST : https://api.github.com/user/keys GET:
+	 * https://api.github.com/user/keys/{keyId} DELETE: POST :
+	 * https://api.github.com/user/keys/{keyId}
+	 */
+	//SSH key to test with
+	String sshKey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB79G6tPC6IARu5/4ycnJiNTyERhE3fKLbpZrolNadY3";
+	//Temp variable to store keyId
+	int keyId;	
+	//Declare the req and resp specification
 	RequestSpecification requestSpec;
 	ResponseSpecification responseSpec;
 	
-	String sshkey = "ssh-";
-	int id;
-	
 	@BeforeClass
-	public void setUp() {
-		requestSpec = new RequestSpecBuilder().
-						addHeader("Content-Type", "application/json").
-						addHeader("Authorization", "token ").
-						setBaseUri("https://api.github.com").
-						build();
-		
-		responseSpec = new ResponseSpecBuilder().
-				expectStatusCode(200).
-				expectContentType("application/json").				
-				build();
+	public void SetUp() {
+		//Initialize the req specification
+		//Since the below parameters are common to all the 3 request, that's what we include in specification
+		requestSpec= new RequestSpecBuilder()
+				.setBaseUri("https://api.github.com/user/keys")
+				.addHeader("Content-type", "application/json")
+				.addHeader("Authorization", "token")
+				.addHeader("X-GitHub-Api-Version", "2022-11-28")
+				.build();
+		//Initialize the response specification
+		responseSpec= new ResponseSpecBuilder()
+				.expectBody("title",Matchers.equalTo("TestKey"))
+				.expectBody("key",Matchers.equalTo(sshKey))
+				.expectResponseTime(Matchers.lessThanOrEqualTo(3000L))
+				.build(); //expectStatusCode(), expectBody("prop",Matchers.equalTo())
 	}
+	
+	@Test(priority=1)
+	public void postRequestTest() {
+		//create a request Body
+		HashMap<String,String> reqBody= new HashMap<String,String>();
+		reqBody.put("title", "TestKey");
+		reqBody.put("key",sshKey);		
+		//send req, save reponse
+		Response response= RestAssured.given().spec(requestSpec).body(reqBody).log().all().when().post();
+		//Extract the  id from response
+		keyId=response.then().extract().path("id");
+		//Assertions
+		response.then().
+		statusCode(201).
+		spec(responseSpec).log().all();
+		
+	}
+		
+	@Test(priority=2)
+	public void getRequestTest() {
+		RestAssured.given().spec(requestSpec).pathParam("keyId", keyId).
+		when().get("/{keyId}").
+		then().statusCode(200).spec(responseSpec);
+    }
+	
+	@Test(priority=3)
+	public void deleteRequestTest() {
+		RestAssured.given().spec(requestSpec).pathParam("keyId", keyId).
+		when().delete("/{keyId}").
+		then().statusCode(204);
+    }
 
-	@Test(priority = 1)
-	public void addSshKey() {
-		String reqBody = "{"
-				+ "\"title\": \"TestAPIKey\","
-				+ "\"key\": \"" + sshkey + "\""
-				+"}";
-		
-		Response response = RestAssured.given().spec(requestSpec) 
-			.body(reqBody)
-			.log().all()
-		.when().post("/user/keys");
-		
-		id = response.then().extract().path("id");
-		System.out.println(id);
-		response.then().log().all();
-		response.then().statusCode(201);
-	
-	}
-	
-	@Test(priority = 2)
-	public void getSshKey() {
-		RestAssured.given().spec(requestSpec) 
-				.pathParam("keyId", id) 
-				.log().all() 
-			.when()
-				.get("/user/keys/{keyId}") 
-			.then().spec(responseSpec) 
-			    .body("id", Matchers.equalTo(id)) 
-			    .body("title", Matchers.equalTo("TestAPIKey"))
-			    .log().all(); 
-	}
-	
-	@Test(priority = 3)
-	public void deleteSshKey()  throws IOException {
-		RestAssured.given().spec(requestSpec) 
-			.pathParam("keyId", id) 
-			.log().all()
-		.when()
-			.delete("/user/keys/{keyId}") 
-		.then()
-			.statusCode(204)
-			.log().all();
-	}
 }
